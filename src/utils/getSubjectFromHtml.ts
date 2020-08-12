e593bb54f339541671284f3123ae0f5a1c16cfe1
John Carmichael <john@johnine.org>
2020-08-12 20:10:59 +0200

    fix: subject was being printed to some email clients - not as txt attribute

--- src/utils/getSubjectFromHtml.ts
+++ src/utils/getSubjectFromHtml.ts
@@ -2,8 +2,8 @@ import cheerio from 'cheerio';
 
 export default (htmlString: string): string | undefined => {
   const $ = cheerio.load(htmlString);
-  const subject = $('subject').text();
-  if (subject.length > 0) {
+  const subject = $('nunjuck-email-subject').attr('text');
+  if (subject && subject.length > 0) {
     return subject;
   }
   return undefined;
